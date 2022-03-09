292ba09f3abba5957e82757c504d9783e710224d
Michael Craun <michael.craun@gmail.com>
2022-03-09 08:14:42 -0500

    Updated readme with installation and update sections; streamlined unpack and update_recipes scripts

--- unpack.sh
+++ unpack.sh
@@ -1,23 +1,29 @@
+DIR=$1
+
+if [ "$DIR" == "" ]; then
+    echo "An installation directory is required, please try again!"
+    exit
+fi
+
 echo "Unpacking custom recipes..."
 
 for file in *
 do
-    if [ $file == "unpack.sh" ] 
-    then
+    if [ $file == "unpack.sh" ]; then
         echo "Skipping unpack.sh..."
-    elif [ $file == "newpack.sh" ] 
-    then
+    elif [ $file == "newpack.sh" ]; then
         echo "Skipping newpack.sh..."
-    elif [ $file == "README.md" ]
-    then 
+    elif [ $file == "README.md" ]; then 
         echo "Skipping README.md..."
-    elif [ $file == "src" ]
-    then
+    elif [ $file == "src" ]; then
         echo "Skipping src directory..."
-    elif [ $file == "update.sh" ]
-    then 
-        echo "Skipping update.sh..."
+    elif [ $file == "update.sh" ]; then 
+        cp -r "$file" "$DIR"
+    elif [ $file == "update_recipes.sh" ]; then
+        cp -r "$file" "$DIR"
     else
-        cp -r $file ..
+        cp -r "$file" "$DIR"
     fi
 done
+
+rm -rf "$( cd "$( dirname "$0" )" && pwd )"
